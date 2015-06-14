bdd41e964f1d7b32da1c57abe3b4179b7bd715a5
Tony Edwards <tony@plymouthsoftware.com>
2015-06-14 10:32:21 +0100

    Adds festivals to the layout sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  $sidebar_festivals = Festival.all.order('date').limit('4')
+
+
+
 end
