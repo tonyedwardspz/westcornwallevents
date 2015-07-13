5db4e33e9cd7ef49aafc0ce38285e8e48c68bf95
Tony Edwards <tony@plymouthsoftware.com>
2015-07-13 22:30:43 +0100

    Prevents deletion of all admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :authorize
   $sidebar_festivals = Festival.all.order('date').limit('4')
 
-  before_action :authorize
+  def not_found
+  raise ActionController::RoutingError.new('Not Found')
+end
 
   protected
   def authorize
