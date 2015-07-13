6da1f5232d62dfb82c01867b58c5b6863b3c7873
Tony Edwards <tony@plymouthsoftware.com>
2015-07-13 22:05:03 +0100

    adds user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to index, notice: "Logged out"
+    redirect_to "/", notice: "Logged out"
   end
 end
--- app/controllers/submit_event_controller.rb
+++ app/controllers/submit_event_controller.rb
@@ -1,4 +1,5 @@
 class SubmitEventController < ApplicationController
+  skip_before_action :authorize, only: [:index]
   def index
     @page_title = 'Submit Event'
   end
