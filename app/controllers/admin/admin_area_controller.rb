4a224ab0341d2b22ec40a0cf0faaf2faf411d76a
Tony Edwards <tony.edwards@gmail.com>
2015-07-15 11:24:19 +0100

    Adds admin area
    Partialises the admin area.
    Started the conversion to rails/slim

---
+++ app/controllers/admin/admin_area_controller.rb
@@ -0,0 +1,3 @@
+class Admin::AdminAreaController < ApplicationController
+  layout 'layouts/admin'
+end
---
+++ test/controllers/admin/admin_area_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::AdminAreaControllerTest < ActionController::TestCase
+  def test_sanity
+    flunk "Need real tests"
+  end
+end
