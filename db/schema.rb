2176b0750c956ca1899f7043349675b72f4a9795
Tony Edwards <tony.edwards@gmail.com>
2015-07-17 18:07:39 +0100

    Add migration for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715103301) do
+ActiveRecord::Schema.define(version: 20150717170552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,10 +65,8 @@ ActiveRecord::Schema.define(version: 20150715103301) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "name"
+    t.string "password_digest"
   end
 
   create_table "venues", force: :cascade do |t|
