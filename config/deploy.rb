9f86f9efb54e55bac8315f393e5ba6fb6e59d202
Tony Edwards <tony.edwards@gmail.com>
2015-07-17 20:29:19 +0100

    Adds sitemap

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :rails_env, ‘production’
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
