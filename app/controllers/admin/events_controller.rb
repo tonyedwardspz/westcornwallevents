64eb4c75045cab9fc9dfdec75c9a34a2a5138b01
Tony Edwards <tony.edwards@gmail.com>
2015-07-21 21:57:51 +0100

    Adds delete event link

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -29,10 +29,8 @@ class Admin::EventsController < Admin::AdminAreaController
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class Admin::EventsController < Admin::AdminAreaController
     respond_to do |format|
       if @event.update(event_params)
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,8 +52,7 @@ class Admin::EventsController < Admin::AdminAreaController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to admin_events_url, notice: 'Event was successfully destroyed.' }
     end
   end
 
