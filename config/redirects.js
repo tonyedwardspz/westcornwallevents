ab3873a51622b9b93674d994dc2a1d298e596292
Tony Edwards <tony.edwards@gmail.com>
2015-07-22 16:35:05 +0100

    Adds new images to project

--- config/redirects.js
+++ config/redirects.js
@@ -1,63 +1,54 @@
 // Festivals
-location ~* /festival/penzance_mazey_day_golowan\.(php|html)$ {
+location = /festival/penzance_mazey_day_golowan.php {
   return 301 /festivals/mazey-day-and-golowan-festival;
 }
 location = /festival/helston_flora_day.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /festivals/helston-flora-day;
 }
 location = /festival/montol_festival_penzance.php {
   return 301 /festivals/mazey-day-and-golowan-festival;
 }
 location = /festival/newlyn_fish_festival.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /festivals/newlyn-fish-festival;
 }
 location = /festival/st_just_lafrowda_festival.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 festivals/lafrowda-festival;
 }
 location = /festival/firework_displays.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /;
 }
 location = /festival/christmas_lights.php {
-	return 301 /festivals/mazey-day-and-golowan-festival;
+	return 301 /;
 }
-
-
-// Venues
 location = /venues/minack_theatre.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /venues/the-minack-theatre;
 }
-
-
-// Pages
 location = /about.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /;
 }
-
-
-// Months
 location = /events_2014/july.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=July&year=2014;
 }
 location = /events_2014/october.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=October&year=2014;
 }
 location = /events_2014/november.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=November&year=2014;
 }
 location = /events_2014/august.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=August&year=2014;
 }
 location = /events_2014/september.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=September&year=2014;
 }
 location = /events_2015.php {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?year=2015;
 }
 location = /events_2013/june.html {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=June&year=2013;
 }
 location = /events_2011/december.html {
-  return 301 /festivals/mazey-day-and-golowan-festival;
+  return 301 /events?month=December&year=2011;
 }
 
 
