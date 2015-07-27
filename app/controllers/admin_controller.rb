c71367f17b5c369ac898ea412c7fcbb1b391ec59
Tony Edwards <tony.edwards@gmail.com>
2015-07-27 19:28:35 +0100

    Adds statitics and recent event changes to dash

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,12 @@
 class AdminController < ApplicationController
   def index
     @total_events = Event.count
+    @total_future_events = Event.where("date >= ?", Date.today).count
+    @total_venues = Venue.count
+    @total_festivals =Festival.count
+    @total_pages = Page.count
+    @total_users = User.count
+    @recently_added_events = Event.order('updated_at DESC').limit(10)
     @new_events = 13
     @page_title = "Admin | West Cornwall Events"
   end
