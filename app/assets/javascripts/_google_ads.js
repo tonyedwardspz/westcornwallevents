4ce20e3f35840ae40feaed682de143c0c38e172e
Tony Edwards <tony.edwards@gmail.com>
2015-07-28 23:43:46 +0100

    Adds conditional sidebar advert

--- app/assets/javascripts/_google_ads.js
+++ app/assets/javascripts/_google_ads.js
@@ -1,22 +1,15 @@
 
-
-
-
 var googleAdvertsInit = function(){
-	console.log("Ads by google");
-
 	// Store advert string
 	var responsiveAd = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7067632003386523" data-ad-slot="3266367277" data-ad-format="auto"></ins>';
 
-	// get refrence to the adverts containers
-	var bodyContainer = $(".google-ad-body");
-	// insert the add into div
-	$(bodyContainer).append(responsiveAd);
+	// get refrence to the adverts containers and insert the add into div
+	$(".google-ad-body").append(responsiveAd);
 
+	if (window.innerWidth > 650 && $(document.height > 1000)){
+      $('#sideSlot').append(responsiveAd)
+  }
 
 	// fire off googles own insert method
 	(adsbygoogle = window.adsbygoogle || []).push({});
 }
-
-
-
