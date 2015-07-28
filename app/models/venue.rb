f5178bf65af0e56fa464931500f7a8e67d8039b2
Tony Edwards <tony.edwards@gmail.com>
2015-07-28 22:53:41 +0100

    Adds validation to venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,9 @@
 class Venue < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name,  use: [:slugged, :finders]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :postcode, presence: true
+  validates :url, presence: true
   has_many :events
 end
