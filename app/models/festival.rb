b54d76a37644300d3e9e5720c86dbdffe00825ae
Tony Edwards <tony.edwards@gmail.com>
2015-07-29 20:57:22 +0100

    Mount image uploader

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,5 +1,7 @@
 class Festival < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
+  mount_uploader :image, AttachmentUploader
+  mount_uploader :image2, AttachmentUploader
   has_many :events
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,7 @@
 class Venue < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name,  use: [:slugged, :finders]
+  mount_uploader :image, AttachmentUploader
   validates :name, presence: true
   validates :address, presence: true
   validates :postcode, presence: true
