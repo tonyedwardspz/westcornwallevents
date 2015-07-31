641cdbd15c191ef8c4782851b106c95c27095d11
Tony Edwards <tony.edwards@gmail.com>
2015-07-31 10:21:11 +0100

    Fix date navigation bug
    Prevents the monthly navigation from useing the wront year.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,9 @@
 module ApplicationHelper
-  def youtube_parse(html)
-    #[youtube ""]
-    matches = html.match /\[youtube "([a-zA-Z0-9_-]*)"\]/i
-
-    return unless matches.size == 2
-
-    youtube_code = matches[1]
-
-
-    injected_html = <<-EOF
-  <div class="video-container">
-    <iframe src="http://www.youtube.com/embed/#{youtube_code}" frameborder="0" width="560" height="315"></iframe>
-  </div>
-EOF
-
-    html.gsub /\[youtube "#{youtube_code}"\]/i, injected_html
+  def getYear
+    if params[:year]
+      @choosen_year = params[:year]
+    else
+      @choosen_year = DateTime.now.year
+    end
   end
 end
