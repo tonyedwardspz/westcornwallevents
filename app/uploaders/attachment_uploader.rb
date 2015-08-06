45aa6a9848c8810adcef96e0d2591aa07b42a025
Tony Edwards <tony.edwards@gmail.com>
2015-08-07 00:51:49 +0100

    Adds image thumbs on upload

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -4,7 +4,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -14,6 +14,10 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "eventimages/"
+  end
+
+  version :thumb do
+    process resize_to_limit: [250,nil]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
