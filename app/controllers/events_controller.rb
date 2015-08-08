f699887d6ad39df4fb13bbfe5d6ce928a056d253
Tony Edwards <tony.edwards@gmail.com>
2015-08-08 01:22:09 +0100

    Fixes variable name conflict

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,7 +32,8 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @page_title = @event.title
-    @page_title.gsub! "&", "%26"
+    @page_meta_title = @event.title
+    @page_meta_title.gsub! "&", "%26"
     if @event.image_link.present?
       @image_meta_url = "http://westcornwallevents.co.uk"
       @image_meta_url << @event.image_link.thumb.to_s
