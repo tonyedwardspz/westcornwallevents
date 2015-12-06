4f6a5a4c630f4766c6f02bad312b58fdff903778
Tony Edwards <tony.edwards@gmail.com>
2015-12-06 07:01:30 +0000

    Working mailchimp

--- app/jobs/subscribe_job.rb
+++ app/jobs/subscribe_job.rb
@@ -3,7 +3,6 @@ class SubscribeJob
 
   def perform(user_event)
     ActiveRecord::Base.connection_pool.with_connection do
-      # user = User.find(id)
       list_id = Rails.application.secrets.mailchimp_list_id
       email = user_event.user_email
       first_name = user_event.first_name
@@ -13,7 +12,6 @@ class SubscribeJob
         g = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
         g.lists(list_id).members.create(body: {email_address: email, status: "subscribed", merge_fields: {FNAME: first_name, LNAME: last_name}, send_welcome: true})
 
-        # SubscribeMailer.confirmation_email(user).deliver
       rescue Gibbon::MailChimpError => mce
         # SuckerPunch.logger.error("subscribe failed: due to #{mce.message}")
         raise mce
