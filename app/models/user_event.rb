9107038696f3f95fcd4bfa2b1edf349660329297
Tony Edwards <tony.edwards@gmail.com>
2015-12-06 10:31:09 +0000

    Adds required fields

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -1,2 +1,12 @@
 class UserEvent < ActiveRecord::Base
+  mount_uploader :image, UsereventAttachmentUploader
+
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
+  validates :description, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_email, presence: true
 end
