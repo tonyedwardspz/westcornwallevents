db43f4f3ac35dce7198dbdce6dd9d2f7d0b0523c
Tony Edwards <tony.edwards@gmail.com>
2015-12-06 10:31:52 +0000

    Adds user events uploader

---
+++ app/uploaders/userevent_attachment_uploader.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+class UsereventAttachmentUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "eventimages/"
+  end
+
+  version :thumb do
+    process resize_to_limit: [250,nil]
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
+  # def url(options = {})
+  #   url = super(options)
+  #   url = url.gsub(/\/uploads\/http%3A\//) if url =~ /http/
+  #   return url
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  def filename
+    o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+    string = (0...50).map { o[rand(o.length)] }.join
+    "#{string}#{original_filename}" if original_filename
+  end
+
+end
