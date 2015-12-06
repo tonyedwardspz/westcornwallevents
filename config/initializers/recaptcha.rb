109c362d8ba99afdb875caba77ac67767e0b358a
Tony Edwards <tony.edwards@gmail.com>
2015-12-06 10:36:30 +0000

    Remove deprecated call

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,5 +3,4 @@ Recaptcha.configure do |config|
   config.public_key = ENV['RECAPTCHA_PUBLIC']
   config.private_key = ENV['RECAPTCHA_PRIVATE']
   config.proxy = 'http://www.google.com/recaptcha/api/verify'
-  config.api_version = 'v2'
 end
