b8bad98ed1a63758b98de051acac34a579e8d6ba
Tony Edwards <tony.edwards@gmail.com>
2015-12-06 03:01:42 +0000

    Adds job for adding user to mailchimp

--- app/jobs/subscribe_job.rb
+++ app/jobs/subscribe_job.rb
@@ -1,15 +1,17 @@
 class SubscribeJob
   # include SuckerPunch::Job
 
-  def perform(email)
+  def perform(user_event)
     ActiveRecord::Base.connection_pool.with_connection do
       # user = User.find(id)
       list_id = Rails.application.secrets.mailchimp_list_id
-      # email = email.email
+      email = user_event.user_email
+      first_name = user_event.first_name
+      last_name = user_event.last_name
 
       begin
         g = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-        g.lists(list_id).members.create(body: {email_address: email, status: "subscribed", merge_fields: {FNAME: "Bob", LNAME: "Smith"}})
+        g.lists(list_id).members.create(body: {email_address: email, status: "subscribed", merge_fields: {FNAME: first_name, LNAME: last_name}, send_welcome: true})
 
         # SubscribeMailer.confirmation_email(user).deliver
       rescue Gibbon::MailChimpError => mce
