df1901186fee15ca994da289935a37f706727ecb
Tony Edwards <tony.edwards@gmail.com>
2015-12-07 10:48:46 +0000

    Add recaptcha to events submission form

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -29,16 +29,16 @@ class UserEventsController < ApplicationController
   # POST /user_events.json
   def create
     @user_event = UserEvent.new(user_event_params)
-    puts
-    if @user_event.add_to_mailling_list
-      SubscribeJob.new.perform(@user_event)
-    end
-
-    respond_to do |format|
-      if @user_event.save
+    if verify_recaptcha(model: @user_event) && @user_event.save
+      if @user_event.add_to_mailling_list
+        SubscribeJob.new.perform(@user_event)
+      end
+      respond_to do |format|
         format.html { redirect_to @user_event, notice: 'User event was successfully created.' }
         format.json { render :show, status: :created, location: @user_event }
-      else
+      end
+    else
+      respond_to do |format|
         format.html { render :new }
         format.json { render json: @user_event.errors, status: :unprocessable_entity }
       end
