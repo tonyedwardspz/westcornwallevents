ec7cd2ee0d2d9bc8c26bd4a587870aa5847f5acf
Tony Edwards <tony.edwards@gmail.com>
2015-12-07 10:47:32 +0000

    Update admin landing page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,9 +3,11 @@ class AdminController < ApplicationController
     @total_events = Event.count
     @total_future_events = Event.where("date >= ?", Date.today).count
     @total_venues = Venue.count
-    @total_festivals =Festival.count
+    @total_festivals = Festival.count
     @total_pages = Page.count
     @total_users = User.count
+    @event_users = EventUser.all.count
+    @number_submitted = UserEvent.all.count
     @recently_added_events = Event.order('updated_at DESC').limit(10)
     @new_events = 13
     @page_title = "Admin"
