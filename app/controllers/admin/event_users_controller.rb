5c6b814a01d5d227b758635d09831d90f43a2d8e
Tony Edwards <tony.edwards@gmail.com>
2015-12-07 10:47:54 +0000

    Add event users to admin

---
+++ app/controllers/admin/event_users_controller.rb
@@ -0,0 +1,78 @@
+  class Admin::EventUsersController < Admin::AdminAreaController
+  before_action :set_event_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /admin/users
+  # GET /admin/users.json
+  def index
+    @event_users = EventUser.all
+    @page_title = "View all Event Users"
+  end
+
+  # GET /admin/users/1
+  # GET /admin/users/1.json
+  def show
+    @page_title = "View Event User"
+  end
+
+  # GET /admin/users/new
+  def new
+    @event_user = EventUser.new
+    @page_title = "New Event User"
+  end
+
+  # GET /admin/users/1/edit
+  def edit
+    @page_title = "Edit Event User"
+  end
+
+  # POST /admin/users
+  # POST /admin/users.json
+  def create
+    @event_user = EventUser.new(event_user_params)
+
+    respond_to do |format|
+      if @event_user.save
+        format.html { redirect_to @event_user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @event_user }
+      else
+        format.html { render :new }
+        format.json { render json: @event_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin/users/1
+  # PATCH/PUT /admin/users/1.json
+  def update
+    respond_to do |format|
+      if @event_user.update(event_user_params)
+        format.html { redirect_to @event_user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event_user }
+      else
+        format.html { render :edit }
+        format.json { render json: @event_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin/users/1
+  # DELETE /admin/users/1.json
+  def destroy
+    @event_user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_event_users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event_user
+      @event_user = EventUser.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_user_params
+      params.require(:event_user).permit(:first_name, :last_name, :user_email, :subscribed)
+    end
+end
