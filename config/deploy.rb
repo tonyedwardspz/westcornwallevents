47262ee52e6b36494e180ebc92c4e0edd73a171d
Tony Edwards <tony.edwards@gmail.com>
2015-12-08 15:22:08 +0000

    Fixes cap not finding rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,8 +35,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 # Setup Rbenv
+set :rbenv_path, "$HOME/.rbenv"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.2.3'
+set :rbenv_ruby, File.read('.ruby-version').strip
 
 namespace :deploy do
 
