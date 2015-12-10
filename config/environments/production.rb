d3b6fe91f470037f65893e6c9c7778d8c693f48e
Tony Edwards <tony.edwards@gmail.com>
2015-12-10 15:08:18 +0000

    Revert compressor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  require 'uglifier'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -26,10 +25,7 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(
-    # Add new lines
-    :output => { :comments => :none }
-  )
+  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
