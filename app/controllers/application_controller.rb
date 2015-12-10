ba8fa484f1c49f8bab324d0ad55cad5abc039cb1
Tony Edwards <tony.edwards@gmail.com>
2015-12-10 18:55:56 +0000

    Fixes sidebar festival caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authorize
-  $sidebar_festivals = Festival.all.order('date').limit('6')
+  @sidebar_festivals = Festival.all.order('date').limit('6')
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
