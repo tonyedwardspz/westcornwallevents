b8aaf04432a879ac7e0067a823feb93a758cb5f4
Tony Edwards <tony.edwards@gmail.com>
2015-12-15 09:37:33 +0000

    Adds helper for url formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,23 @@ module ApplicationHelper
       @choosen_year = DateTime.now.year
     end
   end
+
+  def format_url(url)
+    if url.start_with?('http://')
+      url.slice! 'http://'
+    end
+
+    if url.start_with?('https://')
+      url.slice! 'https://'
+    end
+
+    if !url.start_with?('www.')
+      url.prepend!('www.')
+    end
+
+    if url.end_with?('/')
+      url.chomp!('/')
+    end
+    url
+  end
 end
