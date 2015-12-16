b0d132d8a20bca46882d0316e293b0c6e56d6bbc
Tony Edwards <tony.edwards@gmail.com>
2015-12-16 11:51:46 +0000

    Fix recreate images

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -72,10 +72,10 @@ class Admin::EventsController < Admin::AdminAreaController
   end
 
   def reprocess_images
-    events = Event.where.not(image_link: 'nil')
+    events = Event.all
     begin
       events.each { |e|
-        e.image_link.recreate_versions!
+        e.image_link.recreate_versions! if  e.image_link.present?
       }
     rescue => e
       logger.warn "WARN: Image reprocess - #{e}"
