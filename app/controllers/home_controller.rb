5b4fb92e1bb61758eb56fe52904b547c466d2901
Tony Edwards <tony.edwards@gmail.com>
2015-12-16 09:15:44 +0000

    Adds homepage event grid markup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authorize, only: [:index]
   def index
     @page_title = 'Your local events calendar'
+    @home_events = Event.where.not(image_link: 'nil').limit(6)
   end
 end
