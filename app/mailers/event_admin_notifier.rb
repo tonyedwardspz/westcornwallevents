b5b821e7711d20f57db340ad238b62e894a31230
Tony Edwards <tony.edwards@gmail.com>
2015-12-16 23:28:26 +0000

    Uses corect template

--- app/mailers/event_admin_notifier.rb
+++ app/mailers/event_admin_notifier.rb
@@ -1,9 +1,10 @@
 class EventAdminNotifier < ApplicationMailer
   default :from => 'admin@westcornwallevents.co.uk'
+  layout 'send_submission_admin_email'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_submission_admin_email(event)
-    @user = user
+    @event = event
     mail( :to => 'tony.edwards@gmail.com',
     :subject => 'A new event has been submitted' )
   end
