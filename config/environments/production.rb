34bbe0ddeed7ccea39dce041d54d12f4c5776f7d
Tony Edwards <tony.edwards@gmail.com>
2015-12-17 00:13:40 +0000

    Try setting smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "http://westcornwallevents.co.uk" }
+  config.action_mailer.smtp_settings = {
+    adress: "http://westcornwallevents.co.uk",
+    port: 587
+
+    enable_starttls_auto: true
+    #authentication: 'login'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
