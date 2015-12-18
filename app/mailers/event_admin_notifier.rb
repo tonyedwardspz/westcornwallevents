0ccc4d986dcc31b5a29e4d8ead5d9f3074c2fd88
Tony Edwards <tony.edwards@gmail.com>
2015-12-18 16:31:50 +0000

    Switches to layout in the right location

--- app/mailers/event_admin_notifier.rb
+++ app/mailers/event_admin_notifier.rb
@@ -1,11 +1,11 @@
 class EventAdminNotifier < ApplicationMailer
-  default :from => 'admin@westcornwallevents.co.uk'
-  layout 'send_submission_admin_email'
+  default from: 'admin@westcornwallevents.co.uk'
+  # layout 'send_submission_admin_email'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_submission_admin_email(event)
     @event = event
-    mail( :to => 'tony.edwards@gmail.com',
-    :subject => 'A new event has been submitted' )
+    mail( to: 'tony.edwards@gmail.com',
+    subject: 'A new event has been submitted' )
   end
 end
