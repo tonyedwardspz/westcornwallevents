19342764b0f5d96ab1912c1183913d71666ad7d0
Tony Edwards <tony.edwards@gmail.com>
2015-12-19 13:14:56 +0000

    Adds logger statement

--- app/mailers/submission_live.rb
+++ app/mailers/submission_live.rb
@@ -2,6 +2,7 @@ class SubmissionLive < ApplicationMailer
   default from: 'thanks@westcornwallevents.co.uk'
 
   def submission_live_email(event)
+    logger.debug "LOGGER: Subbmission Live Email method - #{event.event_user.email}"
     @this_event = event
     mail( to: "#{event.event_user.email}",
     subject: "Your event is now live." )
