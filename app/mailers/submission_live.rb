b3a8d8274c48f8e5c2dd8e01daab644ffebb01ff
Tony Edwards <tony.edwards@gmail.com>
2015-12-19 11:55:05 +0000

    Fix syntax errors

--- app/mailers/submission_live.rb
+++ app/mailers/submission_live.rb
@@ -4,7 +4,7 @@ class SubmissionLive < ApplicationMailer
 
   def submission_live_email(event)
     @this_event = event
-    mail( to: "{event.event_user.email}",
+    mail( to: "#{event.event_user.email}",
     subject: "Your event is now live." )
   end
 end
--- app/mailers/submission_thank_you.rb
+++ app/mailers/submission_thank_you.rb
@@ -5,7 +5,7 @@ class SubmissionThankYou < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def submission_thankyou_email(event)
     @event = event
-    mail( to: "{event.user_email}",
+    mail( to: "#{event.user_email}",
     subject: "Thank you for submitting '#{event.title}'" )
   end
 end
