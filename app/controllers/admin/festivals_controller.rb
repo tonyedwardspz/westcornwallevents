c87e2a3fe8a6af2a8515962f7a78cf9c6d43a304
Tony Edwards <tony.edwards@gmail.com>
2015-12-20 19:01:27 +0000

    Keeps admin in admin area after festival actions

--- app/controllers/admin/festivals_controller.rb
+++ app/controllers/admin/festivals_controller.rb
@@ -29,7 +29,6 @@ class Admin::FestivalsController < Admin::AdminAreaController
   # POST /admin/festivals.json
   def create
     @admin_festival = Festival.new(admin_festival_params)
-    @admin_festival = Event.new(event_params)
     @admin_festival.date = @admin_festival.date.strftime("%m/%d/%Y")
     if @admin_festival.end_date.present?
       @admin_festival.end_date = @admin_festival.end_date.strftime("%m/%d/%Y")
@@ -37,7 +36,7 @@ class Admin::FestivalsController < Admin::AdminAreaController
 
     respond_to do |format|
       if @admin_festival.save
-        format.html { redirect_to @admin_festival, notice: 'Festival was successfully created.' }
+        format.html { redirect_to admin_festival_path(@admin_festival), notice: 'Festival was successfully created.' }
       else
         format.html { render :new }
       end
@@ -49,7 +48,7 @@ class Admin::FestivalsController < Admin::AdminAreaController
   def update
     respond_to do |format|
       if @admin_festival.update(admin_festival_params)
-        format.html { redirect_to @admin_festival, notice: 'Festival was successfully updated.' }
+        format.html { redirect_to admin_festival_path(@admin_festival), notice: 'Festival was successfully updated.' }
       else
         format.html { render :edit }
       end
