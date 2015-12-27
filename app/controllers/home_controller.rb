a02a1c2fdb5d828479df9bcad55bf34a3bc909be
Tony Edwards <tony.edwards@gmail.com>
2015-12-27 15:36:30 +0000

    Changes to scoped query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authorize, only: [:index]
   def index
     @page_title = 'Your local events calendar'
-    @home_events = Event.where('date > ?', DateTime.now).where.not(image_link: 'nil').order('date').limit(6)
+    @home_events = Event.homepage
   end
 end
