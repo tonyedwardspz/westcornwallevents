55bca924c339656f8e834228c8e22e588d82071d
Tony Edwards <tony.edwards@gmail.com>
2015-12-27 17:13:19 +0000

    Adds error states for views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
 
-    if user and user.try(:authenticate, params[:password])
-      session[:user_id] = user.id
-      redirect_to admin_url
+    if verify_recaptcha(model: @user_event)
+      if user and user.try(:authenticate, params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_url
+      else
+        redirect_to login_url, alert: "Invalid user/password combination"
+      end
     else
-      redirect_to login_url, alert: "Invalid user/password combination"
+      redirect_to login_url, alert: "Confirm you are human"
     end
   end
 
@@ -19,4 +23,9 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to "/", notice: "Logged out"
   end
+
+  private
+    def session_params
+      params.require(:session).permit(:name, :password, :authenticity_token)
+    end
 end
