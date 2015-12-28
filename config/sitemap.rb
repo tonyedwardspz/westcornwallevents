cc5ec18b028980f8a0247dcb141b637b47a15a6d
Tony Edwards <tony.edwards@gmail.com>
2015-12-28 16:02:24 +0000

    Add blogs to the sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -35,9 +35,14 @@ SitemapGenerator::Sitemap.create do
     add festival_path(festival), :lastmod => festival.updated_at
   end
 
-  add venues_path, :priority => 0.7, :changefreq => 'weekly'
+  add venues_path, :priority => 0.5, :changefreq => 'weekly'
   Venue.find_each do |venue|
     add venue_path(venue), :lastmod => venue.updated_at
+  end
+
+  add blogs_path, :priority => 0.6, :changefreq => 'weekly'
+  Blog.find_each do |blog|
+    add blog_path(blog), :lastmod => blog.updated_at
   end
 
   add pages_path, :priority => 0.4, :changefreq => 'monthly'
