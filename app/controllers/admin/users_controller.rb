1a2bd3a8da4f3d5d9324892cb8e78fe1d72927ea
Tony Edwards <tony.edwards@gmail.com>
2015-12-28 16:35:51 +0000

    Add working relation between users and posts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -68,6 +68,6 @@
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :password, :password_confirmation, :twitter, :display_name)
     end
 end
