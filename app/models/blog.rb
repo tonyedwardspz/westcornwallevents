ce56159b7a43c43a99f8b3164d8f485f1cf2dbc9
Tony Edwards <tony.edwards@gmail.com>
2016-01-07 18:25:18 +0000

    Adds relation ships to the model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,6 @@ class Blog < ActiveRecord::Base
   mount_uploader :image, AttachmentUploader
   friendly_id :title, use: [:slugged, :finders]
   belongs_to :user
+  has_and_belongs_to_many :festivals
   has_and_belongs_to_many :events
 end
