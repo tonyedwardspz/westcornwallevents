832bc50eef8a6c151189a1e94369f5a76a42c22f
Tony Edwards <tony.edwards@gmail.com>
2016-01-07 23:31:16 +0000

    Adds validation to the festival view

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -5,4 +5,12 @@ class Festival < ActiveRecord::Base
   mount_uploader :image2, AttachmentUploader
   has_many :events
   has_and_belongs_to_many :blogs
+  validates :title, presence: true
+  validates :date, presence: true
+  validates :meta_title, presence: true
+  validates :meta_description, presence: true
+  validates :snippet, presence: true
+  validates :summary, presence: true
+  validates :image_alt, presence: true, if: "image.present?"
+  validates :image_alt2, presence: true, if: "image2.present?"
 end
