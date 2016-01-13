6224193cbb1444254c130713156263449d9d29e6
Tony Edwards <tony.edwards@gmail.com>
2016-01-13 16:12:15 +0000

    Adds filtering to results controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,26 @@ class SearchController < ApplicationController
   skip_before_action :authorize
 
   def results
-    @results = PgSearch.multisearch(params[:q])
+    if params['q'].present?
+      @results = PgSearch.multisearch(params[:q])
+      @search_future_events = []
+      @search_past_events = []
+      @search_blogs = []
+      @search_festivals = []
+      @search_venues = []
+      @results.each do |result|
+        r = result.searchable
+        if r.class.name == "Event"
+          if r.date > DateTime.now
+            @search_future_events << r
+          else
+            @search_past_events << r
+          end
+        end
+      end
+      @page_title = "#{params['q']} - Search Results"
+    else
+      @page_title = "Search Results"
+    end
   end
 end
