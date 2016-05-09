f70a2e1fae2b3343d6baf80f5eda1bf2eeb55d3c
Tony Edwards <tony.edwards@gmail.com>
2016-05-09 22:19:32 +0100

    Remove cache gem

--- config/initializers/service-worker-cache-rails.rb
+++ config/initializers/service-worker-cache-rails.rb
@@ -1,13 +1,13 @@
-ServiceWorkerCacheRails.configure do |config|
-  # Should we enable the cache?
-  # config.enabled = true
-
-  # Name your cache
-  config.cache_name = 'west-cornwall-events-v1'
-
-  # Array of files to be cached
-  config.assets = ['application.css', 'application.js']
-
-  # Show debug messages?
-  config.debug = true
-end
+# ServiceWorkerCacheRails.configure do |config|
+#   # Should we enable the cache?
+#   # config.enabled = true
+#
+#   # Name your cache
+#   config.cache_name = 'west-cornwall-events-v1'
+#
+#   # Array of files to be cached
+#   config.assets = ['application.css', 'application.js']
+#
+#   # Show debug messages?
+#   config.debug = true
+# end
